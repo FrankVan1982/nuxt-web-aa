c9d97613c182b0ecd15fbd97f2263f1a8a117dae
FrankVan1982 <consulenza.vangi@outlook.com>
2024-07-22 10:47:02 +0200

    Add @nuxtjs/google-fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/ui'],
+  modules: ['@nuxt/ui', '@nuxtjs/google-fonts'],
   compatibilityDate: '2024-07-20',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
